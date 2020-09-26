26998ccc149f206b9707fb89c528240278b6ba00
Nate Sales <nate@natesales.net>
2020-09-26 01:03:24 -0700

    create TextInput component

---
+++ src/components/TextInput.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let name, placeholder = "";
+</script>
+
+<main>
+    <label>
+        <input type="text" name={name} placeholder={placeholder}>
+    </label>
+</main>
+
+<style>
+    input {
+        border: 1px solid white;
+        border-radius: 15px;
+        margin: 2px;
+        padding: 5px;
+        background: transparent;
+        color: white;
+    }
+</style>
