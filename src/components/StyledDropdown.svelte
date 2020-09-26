fafb9b4513718fc068bbfd571511e02278529081
Nate Sales <nate@natesales.net>
2020-09-26 01:26:28 -0700

    create StyledDropdown component

---
+++ src/components/StyledDropdown.svelte
@@ -0,0 +1,59 @@
+<style>
+    .dropbtn {
+        color: white;
+        font-size: 16px;
+        cursor: pointer;
+        border: 1px solid white;
+        border-radius: 15px;
+        margin: 2px;
+        padding: 10px;
+        background: transparent;
+    }
+
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        z-index: 1;
+    }
+
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    .dropdown-content a:hover {
+        background-color: #f1f1f1;
+    }
+
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
+
+    .dropdown:hover .dropbtn {
+        background-color: #474747;
+        border-radius: 15px 15px 0 0;
+
+    }
+</style>
+
+<main>
+    <div class="dropdown">
+        <button class="dropbtn">Dropdown</button>
+        <div class="dropdown-content">
+<!--            TODO: Global classes for these -->
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+        </div>
+    </div>
+</main>
